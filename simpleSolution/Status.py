bad45b5d7a9b3a60ecae87e0498cdd62db0a4de0
dannyXSC <874794016@qq.com>
2021-05-10 20:30:32 +0800

    add framework

--- simpleSolution/Status.py
+++ simpleSolution/Status.py
@@ -2,7 +2,6 @@ from Global import *
 
 
 class Status():
-    def __init__(self, r=0, s=0, t=0):
-        self.is_Run = r
+    def __init__(self, s=0, t=0):
         self.state = s
         self.remaining_Time = t
